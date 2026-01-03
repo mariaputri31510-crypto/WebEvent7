bc914b7101e5c657b12d05287e7e49a589343f40
mariaputri31510-crypto <mariaputri31510@gmail.com>
2026-01-03 20:27:16 +0700

    Update logout.php

--- bagian_admin/logout.php
+++ bagian_admin/logout.php
@@ -2,7 +2,7 @@
 // ==================================================
 // Nama File: logout.php
 // Deskripsi: File untuk proses logout admin dengan clean session
-// Dibuat oleh: Aldi Ernando Firmansyah - NIM: 3312511026
+// Dibuat oleh: Maria Putri Agustina Tamba - NIM: 3312511025
 // Tanggal: 22 - 12 - 2025
 // ==================================================
 
